096bacfb87c8f1065bb86a5c6464f9ef95d8c1f0
juneplace <52162532+juneplace@users.noreply.github.com>
2021-06-07 14:26:16 +0200

    javascript for line

---
+++ app.js
@@ -0,0 +1,14 @@
+window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+      document.body.style.setProperty(
+        "--scroll-per",
+        `${(window.pageYOffset / document.body.offsetHeight) * 100}%`
+      );
+    },
+    false
+  );
